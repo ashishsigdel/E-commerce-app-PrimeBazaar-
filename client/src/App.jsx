9d947138ac97dc9f2fad969f24082b5532af0e86
ashish <a.asis.sigdel01@gmail.com>
2024-01-31 20:05:49 +0545

    Add password reset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Signin from "./pages/Signin";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Signup from "./pages/Signup";
+import ResetPassword from "./pages/ResetPassword";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   );
